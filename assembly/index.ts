8733b65741763376e520ff0f886db85da54e65f9
Roger Jungemann <rtaljun@gmail.com>
2025-02-28 23:47:37 -0800

    Buffer stuff

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,60 +1,39 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
-
-export function hello(name: string): string {
-  var prefix = "Hello, ";
-  var suffix = "!";
-  return prefix.concat(name).concat(suffix);
+function ramp(f: f32, t: f32): f32 {
+  return (f * t) % 1.0
 }
 
 function saw(f: f32, t: f32): f32 {
-  return (((f * t * 1.0) % 1.0) - 0.5) * 1.0; // TODO: I think the `* 1.0` should be a `* 2.0`
-}
-
-// TODO: `noise` using FM
-
-// TODO: Do something with this buffer state
-let buf0: f32 = 0.0
-let buf1: f32 = 0.0
-function lores2(f: f32, q: f32, input: f32): f32 {
-  const f2 = f32(Math.min(f, 0.999))
-  const fb: f32 = q + q / (1.0 - f2);
-  buf0 = buf0 + f2 * (input - buf0 + fb * (buf0 - buf1));
-  buf1 = buf1 + f2 * (buf0 - buf1);
-  return buf1
-}
-
-function ramp(f: f32, t: f32): f32 {
-  return (f * t * 1.0) % 1.0;
+  return ramp(f, t) * 2.0 - 1.0
 }
 
 function tri(p: f32, t: f32): f32 {
-  return (t < p) ? t / p : -(t / (1.0 - p)) + (1.0 / (1.0 - p))
+  return (t < p)
+  ? t / p
+  : -(t / (1.0 - p)) + (1.0 / (1.0 - p))
 }
 
-export function setup(): void {
+function lores(buf: Array<f32>, f: f32, q: f32, input: f32): f32 {
+  const f2 = f32(Math.min(f, 0.999))
+  const fb: f32 = q + q / (1.0 - f2);
+  buf[0] = buf[0] + f2 * (input - buf[0] + fb * (buf[0] - buf[1]))
+  buf[1] = buf[1] + f2 * (buf[0] - buf[1])
+  return buf[1]
+}
 
+// ----
+// Main
+// ----
+
+const filterData: Array<f32> = [0.0, 0.0]
+
+export function setup(): void {
 }
 
 export function dsp(t: f32): f32 {
   const detunedSaws: f32 = saw(110, t) + saw(111, t)
   const lfo: f32 = tri(0.5, ramp(0.125, t));
-  const lfo2: f32 = tri(0.01, ramp(0.125, t)) * 0.7;
-  const filtered: f32 = lores2(lfo, 0.7, detunedSaws);
-  return filtered * 0.25;
+  const filtered: f32 = lores(filterData, lfo, 0.7, detunedSaws * 0.5);
+  return filtered * 0.125;
 }
-
-// export function createParameters(): Map<string, Float32Array> {
-//   return new Map();
-// }
-
-// export function parametersGet(config: Map<string, Float32Array>, key: string): Float32Array {
-//   return config.get(key);
-// }
-
-// export function parametersSet(config: Map<string, Float32Array>, key: string, value: Float32Array): Map<string, Float32Array> {
-//   return config.set(key, value);
-// }
